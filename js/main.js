3e88db34b0993b9d765fe209b4681aa87fff1640
Arslan Shirazhiyev <arslanshirazhiyev@gmail.com>
2020-09-01 18:36:17 +0600

    added tabs-script

---
+++ js/main.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// create tabs
+
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+
+
+    // create a function that initially hides all tabs and removes the activity class 
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+
+    // create a function that shows tabs  
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+
+    hideTabContent();
+    showTabContent();
+
+
+   
+// using deligation and creating an event handler on click 
+
+    tabsParent.addEventListener('click', (event) => {
+   
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+
+
+  
+
+});
